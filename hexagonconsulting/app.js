fbabb4cd06308ab8caa3e8b70e9432597f107748
Bennguyenru <135408488+Bennguyenru@users.noreply.github.com>
2025-03-07 12:57:22 +0700

    Add hexagonconsulting/app.js 1741327045726

---
+++ hexagonconsulting/app.js
@@ -0,0 +1,16 @@
+App({
+  globalData: {
+    companyName: 'Hexagon Consulting',
+    version: '1.0.0'
+  },
+  onLaunch() {
+    // App initialization logic
+    console.log('Mini App launched');
+  },
+  onShow() {
+    // Called when the app is shown
+  },
+  onHide() {
+    // Called when the app is hidden
+  }
+});
